84f830c80ab3f9f68937166f2e4302429c7120e7
Adam <tahaadam.pro@gmail.com>
2025-12-19 22:08:44 +0100

    ajout option -i

--- 04-ifshow/scripts/ifshow.c
+++ 04-ifshow/scripts/ifshow.c
@@ -5,11 +5,18 @@
 #include <netinet/in.h>
 
 static void usage(const char *p){
-  fprintf(stderr, "Usage: %s -a\n", p);
+  fprintf(stderr, "Usage: %s -a | %s -i <ifname>\n", p, p);
 }
 
 int main(int argc, char *argv[]){
-  if(argc != 2 || strcmp(argv[1], "-a") != 0){
+  int all = 0;
+  const char *filter = NULL;
+
+  if(argc == 2 && strcmp(argv[1], "-a")==0){
+    all = 1;
+  } else if(argc == 3 && strcmp(argv[1], "-i")==0){
+    filter = argv[2];
+  } else {
     usage(argv[0]);
     return 2;
   }
@@ -23,6 +30,8 @@ int main(int argc, char *argv[]){
     if(!ifa->ifa_addr) continue;
     int fam = ifa->ifa_addr->sa_family;
     if(fam!=AF_INET && fam!=AF_INET6) continue;
+
+    if(!all && filter && strcmp(ifa->ifa_name, filter) != 0) continue;
 
     void *src = (fam==AF_INET)
       ? (void*)&((struct sockaddr_in*)ifa->ifa_addr)->sin_addr
